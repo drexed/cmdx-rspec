559c6dbab5594c0d7caa79759b988cabae078386
Juan Gomez <drexed@users.noreply.github.com>
2025-08-24 12:10:25 -0400

    Init commit

---
+++ lib/cmdx/rspec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "rspec/version"
+
+module Cmdx
+  module Rspec
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/cmdx/rspec_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Cmdx::Rspec do
+  it "has a version number" do
+    expect(Cmdx::Rspec::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
